32b867064d2607f241aa2ce41be5a4255c81d2dc
吴培铭 <1780439574@qq.com>
2025-07-09 20:37:08 +0800

    init: 项目工程初始化

---
+++ packages/reactivity/src/index.ts
@@ -0,0 +1,5 @@
+import { isObject } from '@vue/shared'
+
+export function add(a, b) {
+  return a + b
+}
---
+++ scripts/dev.js
@@ -0,0 +1,59 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { parseArgs } from 'node:util'
+import esbuild from 'esbuild'
+import { createRequire } from 'node:module'
+const {
+  values: { format },
+  positionals,
+} = parseArgs({
+  allowPositionals: true,
+  options: {
+    format: {
+      type: 'string',
+      short: 'f',
+
+      default: 'esm',
+    },
+  },
+})
+
+console.log(format, positionals)
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const require = createRequire(import.meta.url)
+
+const target = positionals.length ? positionals[0] : 'vue'
+
+const entry = resolve(__dirname, `../packages/${target}/src/index.ts`)
+console.log('entry', entry)
+
+/**
+ * --format cjs or esm
+ * cjs => reactive.cjs.js
+ * esm => reactive.esm.js
+ * @type {string}
+ */
+
+const outfile = resolve(
+  __dirname,
+  `../packages/${target}/dist/${target}.${format}.js`,
+)
+
+const pkg = require(`../packages/${target}/package.json`)
+
+esbuild
+  .context({
+    entryPoints: [entry], // 入口文件
+    outfile, // 输出文件
+    format, // 打包格式
+    sourcemap: true, // 开启sourcemap方便调试
+    bundle: true, // 把所有依赖打包到一个文件中
+    globalName: pkg.buildOptions.name,
+  })
+  .then(ctx => {
+    // 监听文件变化
+    return ctx.watch()
+  })
